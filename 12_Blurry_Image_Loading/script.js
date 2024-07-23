0a399a3fcea0a77e7a83031dca3d9f2190cc102a
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-07-23 15:12:29 +0530

    blurry image loading project 12 completed

---
+++ 12_Blurry_Image_Loading/script.js
@@ -0,0 +1,34 @@
+const loadText = document.querySelector('.loading-text')
+const background  = document.querySelector('.bg')
+
+let load = 0 ;
+
+let int =  setInterval(blurring, 30)
+
+function blurring(){
+
+    load++
+
+    if(load > 99){
+        clearInterval(int)
+    }
+
+    loadText.innerHTML = `${load}%`;
+
+    loadText.style.opacity  = scale(load, 0 ,100, 1, 0)
+    background.style.filter  =  `blur(${scale(load, 0 ,100, 30, 0)}px)` 
+
+
+}
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min)  + out_min 
+
+}
+
+
+                    // 50   * -30   / 100 + 30
+                    // -1500 / 100  + 30
+                    // -15 + 30
+                    // 15
