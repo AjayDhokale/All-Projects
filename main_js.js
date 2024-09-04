33f0caa7122db7c183c131ae8489d2685efb4a7e
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-09-04 15:34:38 +0530

    changes

--- main_js.js
+++ main_js.js
@@ -11,3 +11,45 @@
 //       }
 //     });
 //   });
+
+
+const sun = document.querySelector('.bi-sun')
+const moon = document.querySelector('.bi-moon')
+const body  = document.querySelector('body')
+const projects  = document.querySelectorAll('.project-link')
+
+
+sun.addEventListener('click', () => {
+
+    // body.style.backgroundColor = 'black'
+    
+    
+    projects.forEach( pro => {
+        pro.style.backgroundColor = 'white'
+        pro.style.color = 'black'
+    })
+    
+   
+
+        sun.style.display = 'none'
+         moon.style.display = 'inline'
+
+})
+moon.addEventListener('click', () => {
+
+    // body.style.backgroundColor = 'white'
+  
+    
+    projects.forEach( pro => {
+        pro.style.backgroundColor = 'black'
+        pro.style.color = 'white'
+    })
+    
+   
+
+    moon.style.display = 'none'
+    sun.style.display = 'inline'
+
+
+    
+})
