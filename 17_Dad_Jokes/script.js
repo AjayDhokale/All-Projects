884077aee779e771a4a919758f8b9dbb92a06853
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-09-04 17:21:43 +0530

    changes

--- 17_Dad_Jokes/script.js
+++ 17_Dad_Jokes/script.js
@@ -1,14 +1,23 @@
-const jokeE1 = document.querySelector('#joke')
-const jokeBtn = document.querySelector('#jokeBtn')
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokebtn')
 
 jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
+// USING ASYNC/AWAIT
 async function generateJoke() {
-    const res = await fetch('https://official-joke-api.appspot.com/random_joke')
-    const data = await res.json()
-    jokeE1.innerHTML = `${data.setup} - ${data.punchline}`
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
 }
 
 
